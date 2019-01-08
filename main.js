9f46c38b589dfa8f1a8ec199e84e7ddfac015173
Erik Ritter <erik.ritter@caffeine.tv>
2019-01-07 21:37:15 -0800

    Add help text on landing page

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 const rootNode = document.getElementById("root");
 const queryString = location.search.substr(1);
 
-const broadcasts = queryString.split("&");
+const broadcasts = queryString.split("&").filter(string => string);
 
 const wrapperClass = broadcasts.length < 3 ? "two" : "four";
 
-broadcasts.map(broadcast => {
-  var wrapperDiv = document.createElement("div");
-  wrapperDiv.setAttribute("class", "wrapper " + wrapperClass);
-  var iframe = document.createElement("iframe");
-  iframe.setAttribute("id", broadcast + "-iframe");
-  iframe.setAttribute("frameBorder", "0");
-  iframe.setAttribute("src", "https://preview.caffeine.tv/" + broadcast);
-  wrapperDiv.appendChild(iframe);
-  rootNode.appendChild(wrapperDiv);
-});
+if (broadcasts.length === 0) {
+  var helpDiv = document.createElement("div");
+  helpDiv.innerHTML =
+    "Welcome to Multi Caffeine<br /><br />Add up to 4 usernames to the end of the URL in the format: ?ESL&Caffeine&kaph&Ohmwrecker to view multiple broadcasts at once";
+  helpDiv.setAttribute("class", "help");
+  rootNode.appendChild(helpDiv);
+} else {
+  broadcasts.map(broadcast => {
+    var wrapperDiv = document.createElement("div");
+    wrapperDiv.setAttribute("class", "wrapper " + wrapperClass);
+    var iframe = document.createElement("iframe");
+    iframe.setAttribute("id", broadcast + "-iframe");
+    iframe.setAttribute("frameBorder", "0");
+    iframe.setAttribute("src", "https://preview.caffeine.tv/" + broadcast);
+    wrapperDiv.appendChild(iframe);
+    rootNode.appendChild(wrapperDiv);
+  });
+}
